af70db2465815d851d6184ecbea97c0f50dc6500
naveen khuntey <96486321+naveen-khuntey@users.noreply.github.com>
2023-12-07 17:49:51 +0530

    base template

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import Header from './Components/Header'
+import Home from './Components/Home'
+import About from './Components/About'
+import Sponsor from './Components/Sponsor'
+import Footer from './Components/Footer'
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <Header />
+      <Home />
+      <About />
+      <Sponsor />
+      <Footer />
+    </>
   )
 }
 

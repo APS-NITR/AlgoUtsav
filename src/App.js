7b626e67d2b4806814de40db41acb107094285de
Prayant Mohanty <prayant.m@gmail.com>
2023-12-08 14:19:48 +0530

    Bg

--- src/App.js
+++ src/App.js
@@ -4,15 +4,17 @@ import Home from './Components/Home'
 import About from './Components/About'
 import Sponsor from './Components/Sponsor'
 import Footer from './Components/Footer'
+import './App.css'
+
 const App = () => {
   return (
-    <>
+    <div className='back bg-cover bg-scroll'>
       <Header />
       <Home />
       <About />
       <Sponsor />
       <Footer />
-    </>
+    </div>
   )
 }
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,8 @@
 import React from 'react'
+import './Header.css'
 
 export default function Header() {
   return (
-    <div>
-      NAVBAR
-    </div>
+    <></>
   )
 }

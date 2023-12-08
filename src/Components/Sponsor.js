45e09f853f04827ef0bfcfe671a6f064dd2871c7
Devil2709 <abhaumik24@gmail.com>
2023-12-08 14:25:21 +0530

    1st commit

--- src/Components/Sponsor.js
+++ src/Components/Sponsor.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Carroussel from "./Carroussel";
+import Image from "../Images/logo192.png"
+import '../Styles/Sponsors.css'
 
 export default function Sponsor() {
+
+  let cards = [{content: <img src={Image}/>}, {content: <img src={Image}/>}, {content: <img src={Image}/>}, {content: <img src={Image}/>}]
+
   return (
-    <div>
-      Sponsor
+    <div style={{textAlign: "center"}}>
+      <h1 class="text-5xl font-bold">SPONSORS</h1>
+      <Carroussel showArrows={true} cards={cards}/>
     </div>
-  )
+  );
 }

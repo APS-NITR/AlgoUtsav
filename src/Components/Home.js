566d34e53ffc3eb69b53bd38fccb8b05f0c38b82
Prayant Mohanty <prayant.m@gmail.com>
2023-12-18 19:09:54 +0530

    Home page

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,47 @@
 import React from 'react'
+import head from '../assets/AlGoUTsaV.png'
+import greenplanet from '../assets/Home assets/Green Planet.png'
+import spaceshuttle from '../assets/Home assets/Space Shuttle.png'
+import shootingstar from '../assets/Home assets/Shooting star.png'
+import shuttle from '../assets/Home assets/Shuttle.png'
 export default function Home() {
   return (
-    <div style={{display: "flex", justifyContent: "center", width: "100%", alignItems: "center" }} className='text-white'>
-      AlGoUTsaV
+    <div>
+      <div style={{ display: "flex", justifyContent: "center", width: "100%", alignItems: "center" }}>
+        <img
+          src={head}
+          alt="heading"
+          style={{ marginTop: "70px", width: "auto", maxHeight: "140px" }}
+        />
+      </div>
+      <div >
+        <img
+          alt='green planet'
+          src={greenplanet}
+          style={{ position: "absolute", bottom: "0", right: "0", left: "0", maxWidth: "35%" }}
+        />
+      </div>
+      <div >
+        <img
+          alt='Space Shuttle'
+          src={spaceshuttle}
+          style={{ position: "absolute", bottom: "10px", right: "100px", maxWidth: "23%" }}
+        />
+      </div>
+      <div >
+        <img
+          alt='Shooting Star'
+          src={shootingstar}
+          style={{ position: "absolute", top: "0", right: "0", maxWidth: "16%" }}
+        />
+      </div>
+      <div >
+        <img
+          alt='Space Shuttle-2'
+          src={shuttle}
+          style={{ position: "absolute", top: "0", left: "0", maxWidth: "16%" }}
+        />
+      </div>
     </div>
   )
 }

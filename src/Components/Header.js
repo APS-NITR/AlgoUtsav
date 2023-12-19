01dae8c870d55ec4442da2ab0345d06f12ce9d99
naveen khuntey <96486321+naveen-khuntey@users.noreply.github.com>
2023-12-19 10:34:20 +0530

    minor fixes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,10 +4,10 @@ import logo from "../assets/Logo.png";
 export default function Header() {
   return (
     <nav className="flex justify-around items-center gap-2 pt-4 pb-4">
-      <div>
-      <NavLink to="/"><img src={logo} alt="the_logo" className="curs"/></NavLink>
+      <div className="z-20">
+        <NavLink to="/"><img src={logo} alt="the_logo" className="curs"/></NavLink>
       </div>
-      <div className="flex gap-10 pl-[40rem]">
+      <div className="flex gap-10 pl-[40rem] z-20">
         <NavLink to="/"><h1 className="text-sm text-yellow-300 font-pusab curs">HOME</h1></NavLink>
         <NavLink to="about"><h1 className="text-sm text-yellow-300 font-pusab curs">ABOUT</h1></NavLink>
         <NavLink to="sponsor"><h1 className="text-sm text-yellow-300 font-pusab curs">SPONSOR</h1></NavLink>
